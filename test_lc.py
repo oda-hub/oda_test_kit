84bb8a2248ff018caa9dc4ef768bb8ea40d45f2b
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-16 21:51:29 +0200

    scsystem eph

--- test_lc.py
+++ test_lc.py
@@ -6,5 +6,10 @@ def test_timesystem_ct(*a, **aa):
     r = requests.get("https://www.astro.unige.ch/cdci/astrooda/dispatch-data/gw/timesystem/api/v1.0/converttime/REVNUM/2000/IJD")
     print(r.text)
 
+def test_scsystem_ephs(*a, **aa):
+    import requests
+    r = requests.get("https://www.astro.unige.ch/cdci/astrooda/dispatch-data/gw/scsystem/api/v1.0/ephs/UTC/2002-01-01T00:00:00/IJD")
+    print(r.text)
+
 def test_fail(*a, **aa):
     raise Exception("example fail")
