acf5059b62a44033b09032d696b30a80c126a7b9
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-21 21:27:11 +0200

    test image with URI platform

--- test_image.py
+++ test_image.py
@@ -12,7 +12,7 @@ def test_oneimage(cdciplatform, *a, **aa):
 
     if cdciplatform.endswith("production1.2"):
         endpoint = 'www.astro.unige.ch/cdci/astrooda/dispatch-data'
-    elif cdciplatform.endswith("production1.2"):
+    elif cdciplatform.endswith("staging1.2"):
         endpoint = 'http://cdcihn/staging-1.2/dispatch-data'
     else:
         raise Exception("unknown platform")
