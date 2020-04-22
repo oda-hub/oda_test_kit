81518ce1f8da0b7f381ad9c38ac940188ab42e13
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-22 19:05:34 +0200

    scwlist for gw

--- test_gw.py
+++ test_gw.py
@@ -7,3 +7,21 @@ def test_scsystem(*a, **aa):
     import requests
     r = requests.get("https://www.astro.unige.ch/cdci/astrooda/dispatch-data/gw/scsystem/api/v1.0/sc/3000/0/0")
     print(r.text)
+
+
+def test_scwlist(*a, **aa):
+    import requests
+    import time
+    t1 = time.time() - 24*3600*21
+    t2 = time.time() - 24*3600*19
+    s ="https://www.astro.unige.ch/cdci/astrooda/dispatch-data/gw/timesystem/api/v1.0/scwlist/cons/{}/{}?&ra=83&dec=22&radius=200.0&min_good_isgri=1000".format(
+            time.strftime("%Y-%m-%dT%H:00:00", time.gmtime(t1)),
+            time.strftime("%Y-%m-%dT%H:00:00", time.gmtime(t2)),
+        )
+    print(s)
+
+    r = requests.get(s)
+
+    print(r.json())
+
+    assert len(r.json()) > 0
