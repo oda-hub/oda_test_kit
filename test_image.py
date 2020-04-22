73458c741325fa62d7b464d52c49528b0728b0ce
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-22 19:11:46 +0200

    scwlist for gw

--- test_image.py
+++ test_image.py
@@ -38,3 +38,60 @@ def test_oneimage(cdciplatform, *a, **aa):
 
 
 
+def test_2recentscw(cdciplatform, *a, **aa):
+    import requests
+    import time
+    t1 = time.time() - 24*3600*21
+    t2 = time.time() - 24*3600*19
+    s ="https://www.astro.unige.ch/cdci/astrooda/dispatch-data/gw/timesystem/api/v1.0/scwlist/cons/{}/{}?&ra=83&dec=22&radius=200.0&min_good_isgri=1000".format(
+            time.strftime("%Y-%m-%dT%H:00:00", time.gmtime(t1)),
+            time.strftime("%Y-%m-%dT%H:00:00", time.gmtime(t2)),
+        )
+    print(s)
+
+    r = requests.get(s)
+
+    print(r.json())
+
+    scwpick = r.json()[:2]
+
+    print("picked")
+
+    assert len(scwpick) > 0
+
+    print("running test image at ",cdciplatform)
+
+    from oda_api.api import DispatcherAPI
+    from oda_api.plot_tools import OdaImage,OdaLightCurve
+    from oda_api.data_products import BinaryData
+
+    import os
+    from astropy.io import fits
+    import numpy as np
+    from numpy import sqrt
+
+    if cdciplatform.endswith("production1.2"):
+        endpoint = 'www.astro.unige.ch/cdci/astrooda/dispatch-data'
+    elif cdciplatform.endswith("staging1.2"):
+        endpoint = 'http://cdcihn/staging-1.2/frontend/dispatch-data'
+    else:
+        raise Exception("unknown platform")
+
+        
+    disp=DispatcherAPI(host=endpoint)
+
+    print(disp)
+
+
+    data=disp.get_product(instrument='isgri',
+                          product='isgri_image',
+                          scw_list=[s+".001" for s in scwpick],
+                          E1_keV=20,
+                          E2_keV=80,
+                          osa_version='OSA10.2',
+                          RA=0,
+                          DEC=0,
+                          detection_threshold=15,
+                          product_type='Real')
+
+    print(data)
