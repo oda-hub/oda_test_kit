7f864b22eb76eef064ac40ccaa3841d6bf18e61c
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-05-16 22:05:56 +0200

    new versions

--- test_image.py
+++ test_image.py
@@ -1,4 +1,4 @@
-def test_oneimage(cdciplatform, *a, **aa):
+def test_oneimage(cdciplatform, osaversion, *a, **aa):
     print("running test one image at ",cdciplatform)
 
     from oda_api.api import DispatcherAPI
@@ -38,7 +38,7 @@ def test_oneimage(cdciplatform, *a, **aa):
 
 
 
-def test_2recentscw(cdciplatform, timestamp=None, *a, **aa):
+def test_2recentscw(cdciplatform, timestamp=None, osaversion="osa10.2", *a, **aa):
     import requests
     import time
 
@@ -88,13 +88,19 @@ def test_2recentscw(cdciplatform, timestamp=None, *a, **aa):
 
     print(disp)
 
+    if '10.2' in osaversion:
+        osa_version='OSA10.2'
+    elif '11.0' in osaversion:
+        osa_version='OSA11.0'
+    else:
+        osa_version='OSA10.2' # default
 
     data=disp.get_product(instrument='isgri',
                           product='isgri_image',
                           scw_list=[str(s)+".001" for s in scwpick],
                           E1_keV=20,
                           E2_keV=80,
-                          osa_version='OSA10.2',
+                          osa_version=osa_version,
                           RA=0,
                           DEC=0,
                           detection_threshold=15,
