90fafa5aae38b99c525fd9785b779b472c6c0492
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2021-01-29 00:01:11 +0100

    fdqns

--- test_image.py
+++ test_image.py
@@ -4,7 +4,7 @@ def platform_endpoint(cdciplatform):
     if cdciplatform.endswith("production1.2"):
         endpoint = 'www.astro.unige.ch/cdci/astrooda/dispatch-data'
     elif cdciplatform.endswith("staging1.2"):
-        endpoint = 'http://cdcihn/staging-1.2/frontend/dispatch-data'
+        endpoint = 'http://cdcihn.isdc.unige.ch/staging-1.2/frontend/dispatch-data'
     elif cdciplatform.endswith("staging1.3"):
         endpoint = 'http://in.internal.odahub.io/staging-1-3/dispatcher'
     else:
