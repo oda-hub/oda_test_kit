593ad7f2aabdf5513d76fed3988fc3cfe8487189
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2022-04-05 11:54:19 +0200

    test resolver crab, gw, grb

--- test_resolver.py
+++ test_resolver.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+import pprint
 from odaexperiments.run import test_func
 
 logging.basicConfig(level="DEBUG")
@@ -20,7 +21,41 @@ def test_crab(cdciplatform, *a, **aa):
     print('endpoint', endpoint)
 
     R = requests.get(endpoint + "/resolver/api/v1.1/byname/Crab").json()
-    print(R)
+    pprint.pprint(R)
 
     assert abs(R['ra'] - 83.63) < 0.1
     assert abs(R['dec'] - 22.01) < 0.1
+
+
+def test_gw(cdciplatform, *a, **aa):
+    print("running test one image at ", cdciplatform)
+
+    endpoint = platform_endpoint(cdciplatform)
+
+    print('endpoint', endpoint)
+
+    R = requests.get(endpoint + "/resolver/api/v1.1/byname/GW170814").json()
+    pprint.pprint(R)
+
+    assert R['utc'] == "2017-08-14T10:30:43.500"
+
+    R = requests.get(endpoint + "/resolver/api/v1.1/byname/GW190425").json()
+    pprint.pprint(R)
+
+    assert R['utc'] == "2019-04-25T08:18:05.000"
+    assert R['duration'] == 5
+
+
+def test_grb(cdciplatform, *a, **aa):
+    print("running test one image at ", cdciplatform)
+
+    endpoint = platform_endpoint(cdciplatform)
+
+    print('endpoint', endpoint)
+
+    R = requests.get(endpoint + "/resolver/api/v1.1/byname/GRB170105A").json()
+    pprint.pprint(R)
+
+    assert R['utc'] == "2017-01-05T06:14:07.00"
+    assert R['duration'] == 2
+
