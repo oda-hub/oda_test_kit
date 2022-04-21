3ccaf780084098f93b927f39ff766b9523556f59
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2022-04-21 17:11:13 +0200

    add grb

--- test_legacysurvey.py
+++ test_legacysurvey.py
@@ -65,3 +65,31 @@ def test_ls_ph(cdciplatform, *args, **kwargs):
     data_collection = disp.get_product(**par_dict)
     
     data_collection.show()
+
+
+def test_ls_ph_grb(cdciplatform, *args, **kwargs):
+    endpoint = platform_endpoint(cdciplatform) + "/dispatch-data"
+    
+    print('Dispatcher at:', endpoint)
+    
+    from oda_api.api import DispatcherAPI
+    
+    disp = DispatcherAPI(url=endpoint)
+    
+    par_dict={
+        "DEC": -0.013294166666667,
+        "RA": 40.66962125,
+        "T1": "2017-03-06T13:26:48.000",
+        "T2": "2017-03-06T15:32:27.000",
+        "T_format": "isot",
+        "data_release": 9,
+        "instrument": "legacysurvey",
+        "product": "legacy_survey_photometry",
+        "product_type": "Real",
+        "radius_photometry": 1.0,
+        "src_name": "GRB170405"
+    }
+
+    data_collection = disp.get_product(**par_dict)
+    
+    data_collection.show()
